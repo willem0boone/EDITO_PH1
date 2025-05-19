602673c4af73b19585e7734475f7935c2ea64ced
Willem Boone <willem.boone@vliz.be>
2025-05-19 09:37:18 +0200

    deploy files

---
+++ deploy/deploy_edito.sh
@@ -0,0 +1,31 @@
+# Print a message indicating the start of the initialization
+#!/bin/bash
+
+# Print a message indicating the start of the initialization
+echo "Starting initialization..."
+
+# Install the necessary R packages
+R -e 'if(!require("pak"))install.packages("pak")'
+R -e '
+packages <- c(
+"reticulate",
+"testthat",
+"dplyr",
+"ggplot2",
+"stac",
+"yaml",
+"arrow",
+"rstac",
+"tibble",
+"tidyverse",
+"data.table",
+"janitor",
+"pracma",
+"broom",
+"EnvStats",
+"patchwork",
+"rnaturalearth",
+"zoo"
+)
+
+pak::pkg_install(packages)
---
+++ deploy/readme.md
@@ -0,0 +1,25 @@
+# Deploy PLET
+Use these script to install dependencies. There are 2 options
+- Shell install
+- R install
+
+## Shell
+Shell script for deploy on EDITO data lab in R container.
+<br>
+Files:
+```
+deploy_edito.sh
+```
+
+
+## R script
+List of packages and R install script.
+<br>
+Files:
+```
+dependencies.txt
+```
+
+```
+install_dependencies.R
+```
